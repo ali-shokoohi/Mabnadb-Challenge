9493a8ce117d4f5b2b82a8f715738bae386bbd5f
Ali Shokoohi <shokoohi@tutanota.com>
2021-09-05 14:33:31 +0430

    Testing ReturnAllInstruments response body type

--- controllers/instrument_test.go
+++ controllers/instrument_test.go
@@ -1,12 +1,14 @@
 package controllers_test
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"gitlab.com/shokoohi/mabnadp-challenge/models"
 	"gitlab.com/shokoohi/mabnadp-challenge/routers"
 )
 
@@ -14,7 +16,7 @@ func TestReturnAllInstruments(t *testing.T) {
 	// Get router object from our router package
 	router := routers.GetRouter()
 	// Create a http request
-	request, err := http.NewRequest("GET", "/", nil)
+	request, err := http.NewRequest("GET", "/instruments", nil)
 	// Check we have no error
 	assert.Nil(t, err, "Request error must be Nil!")
 	response := httptest.NewRecorder()
@@ -23,4 +25,8 @@ func TestReturnAllInstruments(t *testing.T) {
 	assert.Equal(t, 200, response.Result().StatusCode, "Response code be: "+fmt.Sprint(200)+
 		"! But It's: "+fmt.Sprint(response.Result().StatusCode)+"!!!")
 
+	// Checks response type via convert response body type from []bytes to the []Instrument model type
+	var result []models.Instrument
+	err = json.Unmarshal(response.Body.Bytes(), &result)
+	assert.Nil(t, err, "Can't convert response body type to []Instrument model type")
 }

419fd0f097a0b214eb64488682ad5424dac041a5
Ali Shokoohi <shokoohi@tutanota.com>
2021-09-05 23:13:05 +0430

    Write a unit test for ReturnSingleTrade controller with testify

--- controllers/trade_test.go
+++ controllers/trade_test.go
@@ -30,3 +30,22 @@ func TestReturnAllTrades(t *testing.T) {
 	err = json.Unmarshal(response.Body.Bytes(), &result)
 	assert.Nil(t, err, "Can't convert response body type to []Trade model type")
 }
+
+// TestReturnSingleTrade - Unit test for ReturnSingleTrade controller
+func TestReturnSingleTrade(t *testing.T) {
+	// Get router object from our router package
+	router := routers.GetRouter()
+	// Create a http request
+	request, err := http.NewRequest("GET", "/trades/1", nil)
+	// Check we have no error
+	assert.Nil(t, err, "Request error must be Nil!")
+	response := httptest.NewRecorder()
+	router.ServeHTTP(response, request)
+	// Check response status code must be 200
+	assert.Equal(t, 200, response.Result().StatusCode, fmt.Sprintf("Response code be: %d ! But It's: %d!!!", 200, response.Result().StatusCode))
+
+	// Checks response type via convert response body type from []bytes to the Trade model type
+	var result models.Trade
+	err = json.Unmarshal(response.Body.Bytes(), &result)
+	assert.Nil(t, err, "Can't convert response body type to Trade model type")
+}

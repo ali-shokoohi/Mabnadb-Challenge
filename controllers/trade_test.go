a0a6640b0a8ea03fb4ca325d136c2a3bb24b6e37
Ali Shokoohi <shokoohi@tutanota.com>
2021-09-05 23:46:12 +0430

    Write a unit test for CreateSingleTrade controller with testify

--- controllers/trade_test.go
+++ controllers/trade_test.go
@@ -1,11 +1,13 @@
 package controllers_test
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"gitlab.com/shokoohi/mabnadp-challenge/models"
@@ -48,4 +50,53 @@ func TestReturnSingleTrade(t *testing.T) {
 	var result models.Trade
 	err = json.Unmarshal(response.Body.Bytes(), &result)
 	assert.Nil(t, err, "Can't convert response body type to Trade model type")
+}
+
+// TestCreateSingleTrade - Unit test for CreateSingleTrade controller
+func TestCreateSingleTrade(t *testing.T) {
+	// Create request body for create a instrument
+	instrument := map[string]string{"name": "Test"}
+	instrumentBody, err := json.Marshal(instrument)
+	assert.Nil(t, err, "Create request body's error must be Nil!")
+	// Get router object from our router package
+	router := routers.GetRouter()
+	// Create a http request
+	request, err := http.NewRequest("POST", "/instruments", bytes.NewBuffer(instrumentBody))
+	// Check we have no error
+	assert.Nil(t, err, "Request error must be Nil!")
+	response := httptest.NewRecorder()
+	router.ServeHTTP(response, request)
+	// Check response status code must be 200
+	assert.Equal(t, 201, response.Result().StatusCode, fmt.Sprintf("Response code be: %d ! But It's: %d!!!", 200, response.Result().StatusCode))
+	// Checks response type via convert response body type from []bytes to the Instrument model type
+	var resultInstrument models.Instrument
+	err = json.Unmarshal(response.Body.Bytes(), &resultInstrument)
+	assert.Nil(t, err, "Can't convert response body type to Instrument model type")
+	// Result values must be equal to request's value
+	assert.Equal(t, instrument["name"], resultInstrument.Name, fmt.Sprintf("Response must be: %#v! But It's: %#v !!!", instrument, resultInstrument))
+	// Create request body for create a trade
+	trade := models.Trade{
+		Open: 1, Close: 1, High: 1, Low: 1, InstrumentID: resultInstrument.ID, DateEn: time.Now(),
+	}
+	tradeBody, err := json.Marshal(trade)
+	assert.Nil(t, err, "Create request body's error must be Nil!")
+	// Get router object from our router package
+	router = routers.GetRouter()
+	// Create a http request
+	request, err = http.NewRequest("POST", "/trades", bytes.NewBuffer(tradeBody))
+	// Check we have no error
+	assert.Nil(t, err, "Request error must be Nil!")
+	response = httptest.NewRecorder()
+	router.ServeHTTP(response, request)
+	// Check response status code must be 200
+	assert.Equal(t, 201, response.Result().StatusCode, fmt.Sprintf("Response code be: %d ! But It's: %d!!!", 200, response.Result().StatusCode))
+	// Checks response type via convert response body type from []bytes to the Trade model type
+	var result models.Trade
+	err = json.Unmarshal(response.Body.Bytes(), &result)
+	assert.Nil(t, err, "Can't convert response body type to Trade model type")
+	// Result values must be equal to request's value
+	assert.Equal(t, trade.Open, result.Open, fmt.Sprintf("Response must be: %#v! But It's: %#v !!!", trade, result))
+	assert.Equal(t, trade.Close, result.Close, fmt.Sprintf("Response must be: %#v! But It's: %#v !!!", trade, result))
+	assert.Equal(t, trade.High, result.High, fmt.Sprintf("Response must be: %#v! But It's: %#v !!!", trade, result))
+	assert.Equal(t, trade.Low, result.Low, fmt.Sprintf("Response must be: %#v! But It's: %#v !!!", trade, result))
 }

c8065a90ba29c8146038c9b60bde9227c7517e8a
Ali Shokoohi <shokoohi@tutanota.com>
2021-09-05 15:03:14 +0430

    Write a unit test for ReturnSingleInstrument controller with testify

--- controllers/instrument_test.go
+++ controllers/instrument_test.go
@@ -12,6 +12,7 @@ import (
 	"gitlab.com/shokoohi/mabnadp-challenge/routers"
 )
 
+// TestReturnAllInstruments - Unit test for ReturnAllInstruments controller
 func TestReturnAllInstruments(t *testing.T) {
 	// Get router object from our router package
 	router := routers.GetRouter()
@@ -22,11 +23,29 @@ func TestReturnAllInstruments(t *testing.T) {
 	response := httptest.NewRecorder()
 	router.ServeHTTP(response, request)
 	// Check response status code must be 200
-	assert.Equal(t, 200, response.Result().StatusCode, "Response code be: "+fmt.Sprint(200)+
-		"! But It's: "+fmt.Sprint(response.Result().StatusCode)+"!!!")
+	assert.Equal(t, 200, response.Result().StatusCode, fmt.Sprintf("Response code be: %d ! But It's: %d!!!", 200, response.Result().StatusCode))
 
 	// Checks response type via convert response body type from []bytes to the []Instrument model type
 	var result []models.Instrument
 	err = json.Unmarshal(response.Body.Bytes(), &result)
 	assert.Nil(t, err, "Can't convert response body type to []Instrument model type")
+}
+
+// TestReturnSingleInstrument - Unit test fro ReturnSingleInstrument controller
+func TestReturnSingleInstrument(t *testing.T) {
+	// Get router object from our router package
+	router := routers.GetRouter()
+	// Create a http request
+	request, err := http.NewRequest("GET", "/instruments/1", nil)
+	// Check we have no error
+	assert.Nil(t, err, "Request error must be Nil!")
+	response := httptest.NewRecorder()
+	router.ServeHTTP(response, request)
+	// Check response status code must be 200
+	assert.Equal(t, 200, response.Result().StatusCode, fmt.Sprintf("Response code be: %d ! But It's: %d!!!", 200, response.Result().StatusCode))
+
+	// Checks response type via convert response body type from []bytes to the []Instrument model type
+	var result models.Instrument
+	err = json.Unmarshal(response.Body.Bytes(), &result)
+	assert.Nil(t, err, "Can't convert response body type to Instrument model type")
 }

cbf45b72980f2836b7a73cc4cb20883ff0f1bf91
Ali Shokoohi <shokoohi@tutanota.com>
2021-09-05 11:44:39 +0430

    Add Trades field to the Instrument gorm database model as has many field

--- models/instrument.go
+++ models/instrument.go
@@ -2,7 +2,9 @@ package models
 
 import "gorm.io/gorm"
 
+// Instrument - Gorm database model
 type Instrument struct {
-	gorm.Model        // It has ID, CreatedAt, UpdatedAt, DeletedAt as default
-	Name       string `gorm:"not null" json:"name"`
+	gorm.Model         // It has ID, CreatedAt, UpdatedAt, DeletedAt as default
+	Name       string  `gorm:"not null" json:"name"`
+	Trades     []Trade `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"trades"`
 }

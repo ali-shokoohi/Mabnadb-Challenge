1914519b8d981cf7f87aa0edf06e33b19e5552cc
Ali Shokoohi <shokoohi@tutanota.com>
2021-09-05 13:06:40 +0430

    Add route for the HomePage controller

--- routers/routers.go
+++ routers/routers.go
@@ -2,12 +2,15 @@ package routers
 
 import (
 	"github.com/gin-gonic/gin"
+	"gitlab.com/shokoohi/mabnadp-challenge/controllers"
 )
 
 // GetRouter - Return a Gin router client
 func GetRouter() *gin.Engine {
 	// Create new router
 	r := gin.New()
+	// Route for "/" endpoint
+	r.Any("/", controllers.HomePage)
 	//return router
 	return r
 }

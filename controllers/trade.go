5083d98785ba75c6cd3a36d92c09c133f6927535
Ali Shokoohi <shokoohi@tutanota.com>
2021-09-05 23:12:18 +0430

    Add ReturnSingleTrade controller to return a single trades

--- controllers/trade.go
+++ controllers/trade.go
@@ -18,3 +18,19 @@ func ReturnAllTrades(c *gin.Context) {
 	// Send response to request
 	c.JSON(http.StatusOK, trades)
 }
+
+// ReturnSingleTrade - Return A single trade objects from database via its ID
+func ReturnSingleTrade(c *gin.Context) {
+	// Get ID from params
+	id, exists := c.Params.Get("id")
+	if !exists {
+		c.AbortWithStatusJSON(http.StatusBadRequest, "You must send id in params!")
+	}
+	// Get database client
+	db := databases.GetPostgres()
+	// Find all instrument objects
+	var trade models.Trade
+	db.First(&trade, id)
+	// Send response to request
+	c.JSON(http.StatusOK, trade)
+}

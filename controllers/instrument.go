4c9b00e6e26ca231234a1d5571a13977cf8f3426
Ali Shokoohi <shokoohi@tutanota.com>
2021-09-05 15:01:42 +0430

    Add ReturnSingleInstruments controllers to one single instrument

--- controllers/instrument.go
+++ controllers/instrument.go
@@ -18,3 +18,19 @@ func ReturnAllInstruments(c *gin.Context) {
 	// Send response to request
 	c.JSON(http.StatusOK, instruments)
 }
+
+// ReturnSingleInstrument - Return A single instrument objects from database via its ID
+func ReturnSingleInstrument(c *gin.Context) {
+	// Get ID from params
+	id, exists := c.Params.Get("id")
+	if !exists {
+		c.AbortWithStatusJSON(http.StatusBadRequest, "You must send id in params!")
+	}
+	// Get database client
+	db := databases.GetPostgres()
+	// Find all instrument objects
+	var instrument models.Instrument
+	db.Preload("Trades").First(&instrument, id)
+	// Send response to request
+	c.JSON(http.StatusOK, instrument)
+}
